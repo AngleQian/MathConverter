4c2a7c68fa97f6ad0346bbe8fc5b181938b91c45
Angle Qian <angleqian01@gmail.com>
2019-01-04 13:21:06 +0800

    Initial Commit

---
+++ MathConverter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MathConverter
+//
+//  Created by Angle Qian on 2019/1/4.
+//  Copyright © 2019 Angle Qian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MathConverter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MathConverter
+//
+//  Created by Angle Qian on 2019/1/4.
+//  Copyright © 2019 Angle Qian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
